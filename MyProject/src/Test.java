7e2bcb5d4e93403179e325cc17f4a3f457469042
Mother <Mother@DESKTOP-BQKHNMA>
2021-03-16 12:22:46 +0530

    Deleted a Statement

--- MyProject/src/Test.java
+++ MyProject/src/Test.java
@@ -4,7 +4,6 @@ public class Test {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello FirstApp ....:((( ");
-		System.out.println("1 FirstApp ....:((( ");
 		System.out.println("2 FirstApp ....:((( ");
 		System.out.println("3 FirstApp ....:((( ");
 		System.out.println("4 FirstApp ....:((( ");
